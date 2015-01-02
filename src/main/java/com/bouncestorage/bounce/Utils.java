347369873b28096099ced33d4ce15864a6075f4a
Andrew Gaul <gaul@bouncestorage.com>
2015-01-02 13:33:21 -0800

    Add utils to crawl blobstore and move blobs

---
+++ src/main/java/com/bouncestorage/bounce/Utils.java
@@ -0,0 +1,152 @@
+/*
+ * Copyright 2015 Bounce Storage <info@bouncestorage.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bouncestorage.bounce;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Date;
+import java.util.Iterator;
+
+import com.google.common.base.Preconditions;
+import com.google.common.hash.HashCode;
+
+import org.jclouds.blobstore.BlobStore;
+import org.jclouds.blobstore.domain.Blob;
+import org.jclouds.blobstore.domain.PageSet;
+import org.jclouds.blobstore.domain.BlobBuilder.PayloadBlobBuilder;
+import org.jclouds.blobstore.domain.StorageMetadata;
+import org.jclouds.blobstore.options.ListContainerOptions;
+import org.jclouds.io.ContentMetadata;
+
+final class Utils {
+    private Utils() {
+        throw new AssertionError("intentionally unimplemented");
+    }
+
+    static Iterable<StorageMetadata> crawlBlobStore(
+            BlobStore blobStore, String containerName) {
+        return new CrawlBlobStoreIterable(blobStore, containerName);
+    }
+
+    private static class CrawlBlobStoreIterable implements Iterable {
+        private final BlobStore blobStore;
+        private final String containerName;
+
+        CrawlBlobStoreIterable(BlobStore blobStore, String containerName) {
+            this.blobStore = Preconditions.checkNotNull(blobStore);
+            this.containerName = Preconditions.checkNotNull(containerName);
+        }
+
+        @Override
+        public Iterator<StorageMetadata> iterator() {
+            return new CrawlBlobStoreIterator(blobStore, containerName);
+        }
+    }
+
+    private static class CrawlBlobStoreIterator implements Iterator {
+        private final BlobStore blobStore;
+        private final String containerName;
+        private Iterator<? extends StorageMetadata> iterator;
+        private String marker;
+
+        CrawlBlobStoreIterator(BlobStore blobStore, String containerName) {
+            this.blobStore = Preconditions.checkNotNull(blobStore);
+            this.containerName = Preconditions.checkNotNull(containerName);
+            advance();
+        }
+
+        private void advance() {
+            ListContainerOptions options = new ListContainerOptions();
+            if (marker != null) {
+                options.afterMarker(marker);
+            }
+            PageSet<? extends StorageMetadata> set = blobStore.list(
+                    containerName, options);
+            marker = set.getNextMarker();
+            iterator = set.iterator();
+        }
+
+        @Override
+        public boolean hasNext() {
+            return iterator.hasNext() || marker != null;
+        }
+
+        @Override
+        public StorageMetadata next() {
+            if (!hasNext()) {
+                advance();
+            }
+            return iterator.next();
+        }
+    }
+
+    // TODO: eventually this should support parallel copies, cancellation, and
+    // multi-part uploads
+    static void copyBlob(BlobStore from, BlobStore to,
+            String containerNameFrom, String containerNameTo, String blobName)
+            throws IOException {
+        Blob blobFrom = from.getBlob(containerNameFrom, blobName);
+        ContentMetadata metadata = blobFrom.getMetadata().getContentMetadata();
+        try (InputStream is = blobFrom.getPayload().openStream()) {
+            PayloadBlobBuilder builder = to.blobBuilder(blobName).payload(is);
+
+            String contentDisposition = metadata.getContentDisposition();
+            if (contentDisposition != null) {
+                builder.contentDisposition(contentDisposition);
+            }
+
+            String contentEncoding = metadata.getContentEncoding();
+            if (contentEncoding != null) {
+                builder.contentEncoding(contentEncoding);
+            }
+
+            String contentLanguage = metadata.getContentLanguage();
+            if (contentLanguage != null) {
+                builder.contentLanguage(contentLanguage);
+            }
+
+            HashCode contentMd5 = metadata.getContentMD5AsHashCode();
+            if (contentMd5 != null) {
+                builder.contentMD5(contentMd5);
+            }
+
+            Long contentLength = metadata.getContentLength();
+            if (contentLength != null) {
+                builder.contentLength(metadata.getContentLength());
+            }
+
+            String contentType = metadata.getContentType();
+            if (contentType != null) {
+                builder.contentType(metadata.getContentType());
+            }
+
+            Date expires = metadata.getExpires();
+            if (expires != null) {
+                builder.expires(expires);
+            }
+
+            to.putBlob(containerNameTo, builder.build());
+        }
+    }
+
+    static void moveBlob(BlobStore from, BlobStore to,
+            String containerNameFrom, String containerNameTo, String blobName)
+            throws IOException {
+        copyBlob(from, to, containerNameFrom, containerNameTo, blobName);
+        from.removeBlob(containerNameFrom, blobName);
+    }
+}
---
+++ src/test/java/com/bouncestorage/bounce/UtilsTest.java
@@ -0,0 +1,127 @@
+/*
+ * Copyright 2015 Bounce Storage <info@bouncestorage.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bouncestorage.bounce;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.InputStream;
+import java.util.Random;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.hash.Hashing;
+import com.google.common.io.ByteSource;
+import com.google.common.net.MediaType;
+import com.google.inject.Module;
+
+import org.jclouds.ContextBuilder;
+import org.jclouds.blobstore.BlobStore;
+import org.jclouds.blobstore.BlobStoreContext;
+import org.jclouds.blobstore.domain.Blob;
+import org.jclouds.blobstore.domain.StorageMetadata;
+import org.jclouds.io.ContentMetadata;
+import org.jclouds.logging.slf4j.config.SLF4JLoggingModule;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public final class UtilsTest {
+    private BlobStoreContext nearContext;
+    private BlobStoreContext farContext;
+    private BlobStore nearBlobStore;
+    private BlobStore farBlobStore;
+    private String containerName;
+
+    @Before
+    public void setUp() throws Exception {
+        containerName = createRandomContainerName();
+
+        nearContext = ContextBuilder
+                .newBuilder("transient")
+                .credentials("identity", "credential")
+                .modules(ImmutableList.<Module>of(new SLF4JLoggingModule()))
+                .build(BlobStoreContext.class);
+        nearBlobStore = nearContext.getBlobStore();
+        nearBlobStore.createContainerInLocation(null, containerName);
+
+        farContext = ContextBuilder
+                .newBuilder("transient")
+                .credentials("identity", "credential")
+                .modules(ImmutableList.<Module>of(new SLF4JLoggingModule()))
+                .build(BlobStoreContext.class);
+        farBlobStore = farContext.getBlobStore();
+        farBlobStore.createContainerInLocation(null, containerName);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        if (nearContext != null) {
+            nearBlobStore.deleteContainer(containerName);
+            nearContext.close();
+        }
+        if (farContext != null) {
+            farBlobStore.deleteContainer(containerName);
+            farContext.close();
+        }
+    }
+
+    @Test
+    public void testBounceBlob() throws Exception {
+        String blobName = "blob";
+        ByteSource byteSource = ByteSource.wrap(new byte[1]);
+        Blob blob = nearBlobStore.blobBuilder(blobName)
+                .payload(byteSource)
+                .contentLength(byteSource.size())
+                .contentMD5(byteSource.hash(Hashing.md5()))
+                .contentType(MediaType.OCTET_STREAM)
+                .build();
+        ContentMetadata metadata = blob.getMetadata().getContentMetadata();
+        nearBlobStore.putBlob(containerName, blob);
+
+        for (StorageMetadata sm : Utils.crawlBlobStore(nearBlobStore,
+                containerName)) {
+            Utils.moveBlob(nearBlobStore, farBlobStore, containerName,
+                    containerName, sm.getName());
+        }
+
+        Blob blob2 = farBlobStore.getBlob(containerName, blobName);
+        ContentMetadata metadata2 = blob2.getMetadata().getContentMetadata();
+        try (InputStream is = byteSource.openStream();
+             InputStream is2 = blob2.getPayload().openStream()) {
+            assertThat(metadata2.getContentDisposition()).isEqualTo(
+                    metadata.getContentDisposition());
+            assertThat(metadata2.getContentEncoding()).isEqualTo(
+                    metadata.getContentEncoding());
+            assertThat(metadata2.getContentLanguage()).isEqualTo(
+                    metadata.getContentLanguage());
+            assertThat(metadata2.getContentLength()).isEqualTo(
+                    metadata.getContentLength());
+            assertThat(metadata2.getContentMD5AsHashCode()).isEqualTo(
+                    metadata.getContentMD5AsHashCode());
+            assertThat(metadata2.getContentType()).isEqualTo(
+                    metadata.getContentType());
+            assertThat(metadata2.getExpires()).isEqualTo(
+                    metadata.getExpires());
+            assertThat(is2).hasContentEqualTo(is);
+        }
+        assertThat(nearBlobStore.blobExists(containerName, blobName))
+                .isEqualTo(false);
+    }
+
+    private static String createRandomContainerName() {
+        return "bounce-" + new Random().nextInt(Integer.MAX_VALUE);
+    }
+}
