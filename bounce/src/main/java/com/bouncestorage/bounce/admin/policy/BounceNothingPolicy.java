6ec4149face17492e98d1c929d3313c0aa6b63cb
Ka-Hing Cheung <khc@bouncestorage.com>
2015-03-31 15:01:00 -0700

    converted BounceServiceTest

--- bounce/src/main/java/com/bouncestorage/bounce/admin/policy/BounceNothingPolicy.java
+++ bounce/src/main/java/com/bouncestorage/bounce/admin/policy/BounceNothingPolicy.java
@@ -5,5 +5,9 @@
 
 package com.bouncestorage.bounce.admin.policy;
 
+import com.bouncestorage.bounce.admin.BouncePolicy;
+import com.google.auto.service.AutoService;
+
+@AutoService(BouncePolicy.class)
 public final class BounceNothingPolicy extends NoBouncePolicy {
 }
