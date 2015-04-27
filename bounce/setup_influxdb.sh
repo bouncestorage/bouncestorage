d5e39b68e772224384a6101017bdad0d50e1ed92
Ka-Hing Cheung <khc@bouncestorage.com>
2015-04-27 16:20:37 -0700

    silent curl output

--- bounce/setup_influxdb.sh
+++ bounce/setup_influxdb.sh
@@ -7,6 +7,6 @@ until [ echo >/dev/tcp/localhost/8086 ]; do
     service influxdb start || true
     sleep 5
 done
-curl -X POST 'http://localhost:8086/db?u=root&p=root' -d '{"name": "bounce"}'
-curl -X POST 'http://localhost:8086/db/bounce/users?u=root&p=root' \
+curl -s -X POST 'http://localhost:8086/db?u=root&p=root' -d '{"name": "bounce"}'
+curl -s -X POST 'http://localhost:8086/db/bounce/users?u=root&p=root' \
     -d '{"name":"bounce", "password":"bounce"}'
