06a51d532c8d5c3827e03b9f65028ffb4b4f0f10
Timur Alperovich <timuralp@bouncestorage.com>
2015-04-30 17:45:35 -0700

    Setup influxdb on first startup.
    As opposed to launching influxdb during the container build phase, we
    configure it at the time of the first startup. This is to work around
    the influxdb bug related changing hostnames:
    https://github.com/influxdb/influxdb/issues/1164
    
    Fixes #174

--- bounce/setup_influxdb.sh
+++ bounce/setup_influxdb.sh
@@ -2,7 +2,14 @@
 
 set -e
 
-/etc/init.d/influxdb start
+setup_file=./setup
+
+service influxdb start
+
+if [ -e $setup_file ]; then
+	exit 0
+fi
+
 until [ echo >/dev/tcp/localhost/8086 ]; do
     service influxdb start || true
     sleep 5
@@ -10,3 +17,5 @@ done
 curl -s -X POST 'http://localhost:8086/db?u=root&p=root' -d '{"name": "bounce"}'
 curl -s -X POST 'http://localhost:8086/db/bounce/users?u=root&p=root' \
     -d '{"name":"bounce", "password":"bounce"}'
+
+touch $setup_file
