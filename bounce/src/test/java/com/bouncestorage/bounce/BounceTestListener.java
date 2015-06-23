c33c5712180dd764422b73f8b412647066ad0969
Timur Alperovich <timuralp@bouncestorage.com>
2015-06-23 13:51:35 -0700

    Instantiate a BounceApplication before all tests.
    Instantiate a BounceApplication class before any tests run to avoid
    conflicts in logger registration.

--- bounce/src/test/java/com/bouncestorage/bounce/BounceTestListener.java
+++ bounce/src/test/java/com/bouncestorage/bounce/BounceTestListener.java
@@ -5,12 +5,20 @@
 
 package com.bouncestorage.bounce;
 
+import com.bouncestorage.bounce.admin.BounceApplication;
 import com.bouncestorage.bounce.admin.ContainerPool;
 
+import org.junit.runner.Description;
 import org.junit.runner.Result;
 import org.junit.runner.notification.RunListener;
 
 public class BounceTestListener extends RunListener {
+    @Override
+    public void testRunStarted(Description description) {
+        // Done to ensure that the BounceApplication class is loaded once
+        new BounceApplication();
+    }
+
     @Override
     public void testRunFinished(Result result) {
         ContainerPool.destroyAllContainers();
