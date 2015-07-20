47817e64e984554477cfe60cb63948b05ebcdbe5
Ka-Hing Cheung <khc@bouncestorage.com>
2015-07-20 14:32:07 -0700

    start bounce from maven

---
+++ bounce/src/test/resources/run_proxy_tests.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -o xtrace
+set -o errexit
+set -o nounset
+
+function cleanup {
+    if [ "$PROXY_PID" != "" ]; then
+        kill $PROXY_PID
+    fi
+}
+
+trap cleanup EXIT
+
+PROXY_BIN="mvn -o exec:java $BOUNCE_OPTS -Dbounce.autoconfig-tiers=true"
+stdbuf -oL -eL $PROXY_BIN -Dexec.args='--properties src/main/resources/bounce.properties' &
+PROXY_PID=$!
+
+export SKIP_PROXY=1
+pushd ../swiftproxy
+sleep 5
+
+SKIP_TESTS="-e test.functional.tests:TestFile.testCopyFromHeader -e test.functional.tests:TestSlo.test_slo_copy_account -e test.functional.tests:TestSlo.test_slo_copy_the_manifest_account -e test.functional.tests:TestFile.testCopy -e test.functional.tests:TestFile.testCopyAccount -e test.functional.tests:TestFile.testCopyFromHeader404s -e test.functional.tests:TestFile.testNameLimit"
+export SKIP_TESTS
+
+
+src/test/resources/run-swift-tests.sh
+exit $?
