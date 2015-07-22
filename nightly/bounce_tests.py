41398e90d7f4b3545ab8c8ba45e8be6488877550
Timur Alperovich <timuralp@bouncestorage.com>
2015-07-22 12:30:05 -0700

    Enable pep8 checking.
    Enables pep8 style checking. pep8 should be installed, otherwise this
    goal will fail.
    
    Also, upgrades the exec-maven-plugin to 1.4.0 released in March, 2015.

--- nightly/bounce_tests.py
+++ nightly/bounce_tests.py
@@ -47,6 +47,7 @@ BOUNCE_NIGHTLY_TEST_NAME = "nightly/bounce_tests.py"
 DOCKER_SWIFT_REPO = "https://github.com/kahing/docker-swift"
 DOCKER_SWIFT_DIR = "docker-swift"
 IMAGE = 'kahing/docker-swift'
+
 SWIFT_DATA_DIR = "/tmp/docker-swift"
 
 ACCESS_KEY_FIELD = 'AccessKeyId'
@@ -57,23 +58,25 @@ BLOBSTORE_2_PROPERTY_PREFIX = 'bounce.backend.1.jclouds'
 
 SSH_KEY_NAME = "/home/admin/.ssh/id_rsa"
 
-PACKAGES = [ 'openjdk-8-jdk git fortune cowsay docker.io' ]
+PACKAGES = ['openjdk-8-jdk git fortune cowsay docker.io']
 APT_SOURCES = "/etc/apt/sources.list"
 UNSTABLE_REPO = "deb http://cloudfront.debian.net/debian unstable main"
 
 VERIFIER_SENDER = "timuralp@bouncestorage.com"
-VERIFIER_RECIPIENTS = ["timuralp@bouncestorage.com",
-                       "khc@bouncestorage.com",
-                       "gaul@bouncestorage.com"
+VERIFIER_RECIPIENTS = [
+                        "timuralp@bouncestorage.com",
+                        "khc@bouncestorage.com",
+                        "gaul@bouncestorage.com",
                       ]
 
 OUTPUT_LOG = '/tmp/bounce_verifier.log'
 
-#JAVA_PROPERTIES = [ '-DLOG_LEVEL=info' ]
-JAVA_PROPERTIES = [  ]
+JAVA_PROPERTIES = []
+
 
 class TestException(BaseException):
     pass
+
 
 class Creds(object):
     def __init__(self, key, secret, token):
@@ -81,12 +84,13 @@ class Creds(object):
         self.secret = secret
         self.token = token
 
-def execute(command, capture = False):
+
+def execute(command, capture=False):
     out = None
     if capture:
         out = ''
     process = subprocess.Popen(command, stderr=subprocess.STDOUT,
-            stdout=subprocess.PIPE, shell=True)
+                               stdout=subprocess.PIPE, shell=True)
     for line in iter(process.stdout.readline, ''):
         if capture:
             out += line.rstrip()
@@ -98,23 +102,28 @@ def execute(command, capture = False):
     if capture:
         return out
 
+
 def git_clone(repo, directory):
     target_dir = os.path.join(os.environ['HOME'], directory)
     if os.path.exists(target_dir):
         execute("cd %s && git pull" % target_dir)
     else:
         execute("echo \"StrictHostKeyChecking\tno\" | tee -a ~/.ssh/config")
-        execute("cd %s && git clone %s %s" % (os.environ['HOME'], repo, directory))
+        execute("cd %s && git clone %s %s"
+                % (os.environ['HOME'], repo, directory))
+
 
 def git_update_submodule(directory):
     target_dir = os.path.join(os.environ['HOME'], directory)
     execute("cd %s && git submodule init" % target_dir)
     execute("cd %s && git submodule update" % target_dir)
 
+
 def setup_code():
     with open(APT_SOURCES) as f:
         if f.read().find("unstable") == -1:
-            execute("echo \"%s\"| sudo tee -a %s" % (UNSTABLE_REPO, APT_SOURCES))
+            execute("echo \"%s\"| sudo tee -a %s"
+                    % (UNSTABLE_REPO, APT_SOURCES))
 
     execute("sudo apt-get update")
     execute("sudo apt-get install -y " + " ".join(PACKAGES))
@@ -123,9 +132,11 @@ def setup_code():
     target_dir = os.path.join(os.environ['HOME'], BOUNCE_SRC_DIR)
     execute("cd %s && mvn install -DskipTests=true" % target_dir)
 
+
 def setup_swift():
     git_clone(DOCKER_SWIFT_REPO, DOCKER_SWIFT_DIR)
     execute("cd ~/%s && sudo docker build -t %s ." % (DOCKER_SWIFT_DIR, IMAGE))
+
 
 def start_docker_swift(datadir):
     if os.path.exists(datadir):
@@ -133,43 +144,60 @@ def start_docker_swift(datadir):
         execute("cd %s && sudo rm -Rf *" % datadir)
         os.chdir(cwd)
     execute("sudo mkdir -p %s" % datadir)
-    container = execute("sudo docker run -d -P -v %s:/swift/nodes -t %s" % (datadir, IMAGE), capture = True)
-    port = execute("sudo docker inspect --format '{{ (index (index .NetworkSettings.Ports \"8080/tcp\") 0).HostPort }}' %s" % container, capture = True)
+
+    container = execute("sudo docker run -d -P -v %s:/swift/nodes -t %s"
+                        % (datadir, IMAGE), capture=True)
+    inspect_template = '{{ (index (index .NetworkSettings.Ports \"8080/tcp\")'\
+                       ' 0).HostPort }}'
+    port = execute("sudo docker inspect --format '%s' %s"
+                   % (inspect_template, container), capture=True)
     return (container, port)
+
 
 def get_file_dir():
     return os.path.dirname(os.path.realpath(__file__))
 
+
 def get_s3_creds():
     props = json.load(urllib.urlopen(META_URL))
-    return Creds(props['AccessKeyId'], props['SecretAccessKey'], props['Token'])
+    return Creds(props['AccessKeyId'], props['SecretAccessKey'],
+                 props['Token'])
+
 
 def get_object(creds, object_name):
-    conn = boto.connect_s3(creds.key, creds.secret, security_token = creds.token)
-    security_headers = { 'x-amz-security-token': creds.token }
-    bucket = conn.get_bucket(CONFIG_BUCKET, validate=False, headers=security_headers)
+    conn = boto.connect_s3(creds.key, creds.secret,
+                           security_token=creds.token)
+    security_headers = {'x-amz-security-token': creds.token}
+    bucket = conn.get_bucket(CONFIG_BUCKET, validate=False,
+                             headers=security_headers)
     return bucket.get_key(object_name, headers=security_headers)
+
 
 def remove_reports():
     target_dir = os.path.join(os.environ['HOME'], SUREFIRE_DIR)
     execute("rm -f %s" % (SUREFIRE_ARCHIVE))
     execute("cd %s && rm -rf %s.*" % (target_dir, SUREFIRE_DIR_NAME))
 
+
 def archive_surefire(provider):
     target_dir = os.path.join(os.environ['HOME'], SUREFIRE_DIR)
     updated_name = SUREFIRE_DIR_NAME + "." + provider
-    execute("cd %s && mv %s %s" % (target_dir, SUREFIRE_DIR_NAME, updated_name))
-    execute("tar -C %s -rvf %s %s" % (target_dir, SUREFIRE_ARCHIVE,
-            updated_name))
+    execute("cd %s && mv %s %s" %
+            (target_dir, SUREFIRE_DIR_NAME, updated_name))
+    execute("tar -C %s -rvf %s %s" %
+            (target_dir, SUREFIRE_ARCHIVE, updated_name))
+
 
 def append_file_to_archive(path):
     execute("tar -rvf %s %s" % (SUREFIRE_ARCHIVE, path))
 
-def compress_archive():
-    execute("cd %s && gzip -f %s" % (os.path.dirname(SUREFIRE_ARCHIVE),
-        SUREFIRE_ARCHIVE))
 
-def send_email(creds, subject, body, attachment = None):
+def compress_archive():
+    execute("cd %s && gzip -f %s" %
+            (os.path.dirname(SUREFIRE_ARCHIVE), SUREFIRE_ARCHIVE))
+
+
+def send_email(creds, subject, body, attachment=None):
     conn = boto.ses.connect_to_region("us-east-1",
                                       aws_access_key_id=creds.key,
                                       aws_secret_access_key=creds.secret,
@@ -182,19 +210,22 @@ def send_email(creds, subject, body, attachment = None):
         msg['To'] = ', '.join(VERIFIER_RECIPIENTS)
         msg.preamble = 'Multipart test results.\n'
         msg.attach(email.mime.text.MIMEText(body))
-        mimeAttachment = email.mime.application.MIMEApplication(open(attachment,
-                'rb').read())
+        mimeAttachment = email.mime.application.MIMEApplication(
+                open(attachment, 'rb').read())
         mimeAttachment.add_header('Content-Disposition', 'attachment',
-                filename=os.path.basename(attachment))
+                                  filename=os.path.basename(attachment))
         msg.attach(mimeAttachment)
-        conn.send_raw_email(msg.as_string(), source = VERIFIER_SENDER,
-                destinations = VERIFIER_RECIPIENTS)
+        conn.send_raw_email(msg.as_string(),
+                            source=VERIFIER_SENDER,
+                            destinations=VERIFIER_RECIPIENTS)
     else:
-        conn.send_email(VERIFIER_SENDER,
+        conn.send_email(
+                        VERIFIER_SENDER,
                         subject,
                         body,
                         VERIFIER_RECIPIENTS
                        )
+
 
 def setup_github_key(creds):
     key = get_object(creds, GITHUB_KEY)
@@ -202,23 +233,33 @@ def setup_github_key(creds):
         key.get_contents_to_file(key_file)
         os.chmod(SSH_KEY_NAME, stat.S_IRUSR | stat.S_IWUSR)
 
+
 def get_all_blobstore_credentials(creds):
     key = get_object(creds, CONFIG_KEY)
     return json.loads(key.get_contents_as_string())
 
+
 def get_all_blobstore_from_argv():
     return json.load(open(sys.argv[1]))
 
+
 def get_java_properties(provider_details, swift_port):
-    blobstore_2 = map(lambda pair: "-D%s.%s=%s" % (BLOBSTORE_2_PROPERTY_PREFIX,
-            pair[0], pair[1]), provider_details.items())
-    blobstore_1 = map(lambda key: "-D%s%s" % (BLOBSTORE_1_PROPERTY_PREFIX, key),
-            get_swift_properties(swift_port))
+    blobstore_2 = map(lambda pair: "-D%s.%s=%s" %
+                      (BLOBSTORE_2_PROPERTY_PREFIX, pair[0], pair[1]),
+                      provider_details.items())
+    blobstore_1 = map(lambda key: "-D%s%s" %
+                      (BLOBSTORE_1_PROPERTY_PREFIX, key),
+                      get_swift_properties(swift_port))
     return ' '.join(blobstore_2 + blobstore_1 + JAVA_PROPERTIES)
 
+
 def get_swift_properties(swift_port):
-    return [ ".provider=openstack-swift", ".endpoint=http://127.0.0.1:%s/auth/v1.0/" % swift_port,
-             ".identity=test:tester", ".credential=testing", ".keystone.credential-type=tempAuthCredentials" ]
+    return [".provider=openstack-swift",
+            ".endpoint=http://127.0.0.1:%s/auth/v1.0/" % swift_port,
+            ".identity=test:tester",
+            ".credential=testing",
+            ".keystone.credential-type=tempAuthCredentials"]
+
 
 def run_bounce_tests(provider_details, swift_port):
     java_properties = get_java_properties(provider_details, swift_port)
@@ -226,6 +267,7 @@ def run_bounce_tests(provider_details, swift_port):
     command = "src/test/resources/run_proxy_tests.sh"
     print(command)
     execute(command)
+
 
 def run_test(provider_details, swift_port, test="all"):
     print "Testing %s" % provider_details['provider']
@@ -238,23 +280,29 @@ def run_test(provider_details, swift_port, test="all"):
     execute(command)
     run_bounce_tests(provider_details, swift_port)
 
+
 def notify_failure(creds, error, backtrace):
     message = "Exception message:\n%s\n" % error.message
     message += backtrace + "\n"
     send_email(creds, "Nightly failed!", message, COMPRESSED_ARCHIVE)
 
+
 def notify_success(creds):
-    message = "Success!\n%s" % subprocess.check_output(["/usr/games/cowsay",
-                subprocess.check_output("/usr/games/fortune")])
+    message = "Success!\n%s" % subprocess.check_output(
+            ["/usr/games/cowsay",
+             subprocess.check_output("/usr/games/fortune")]
+    )
     send_email(creds, "Nightly passed", message, COMPRESSED_ARCHIVE)
+
 
 def hash_file(filename):
     with open(filename, 'rb') as f:
         return hashlib.md5(f.read()).digest()
 
+
 def maybe_update(log):
     updated_file = os.path.join(os.getcwd(), BOUNCE_SRC_DIR,
-        BOUNCE_NIGHTLY_TEST_NAME)
+                                BOUNCE_NIGHTLY_TEST_NAME)
     current_file = sys.argv[0]
     if hash_file(current_file) != hash_file(updated_file):
         print "Updating the test file"
@@ -262,10 +310,10 @@ def maybe_update(log):
         log.close()
         os.execlp("env", "env", "python", current_file)
 
+
 def main():
     ec2 = len(sys.argv) == 1
     test = "all"
-    #os.environ['LOG_LEVEL'] = 'info'
 
     if ec2:
         log = open(OUTPUT_LOG, 'w')
@@ -289,13 +337,18 @@ def main():
             if len(sys.argv) > 2:
                 test = sys.argv[2]
 
-        saio_near_container, swift_near_port = start_docker_swift(SWIFT_DATA_DIR + "-near")
-        saio_far_container, swift_far_port = start_docker_swift(SWIFT_DATA_DIR + "-far")
-        all_creds += [ { "provider" : "openstack-swift",
-                         "identity" : "test:tester",
-                         "credential" : "testing",
-                         "keystone.credential-type" : "tempAuthCredentials",
-                         "endpoint" : "http://127.0.0.1:%s/auth/v1.0/" % swift_far_port } ]
+        saio_near_container, swift_near_port = start_docker_swift(
+            SWIFT_DATA_DIR + "-near")
+        saio_far_container, swift_far_port = start_docker_swift(
+            SWIFT_DATA_DIR + "-far")
+        far_endpoint = "http://127.0.0.1:%s/auth/v1.0/" % swift_far_port
+        all_creds += [{
+                       "provider": "openstack-swift",
+                       "identity": "test:tester",
+                       "credential": "testing",
+                       "keystone.credential-type": "tempAuthCredentials",
+                       "endpoint": far_endpoint
+                      }]
 
         os.chdir("bounce")
         if ec2:
@@ -324,7 +377,10 @@ def main():
         sys.stdout = open(os.devnull, 'w')
         append_file_to_archive(OUTPUT_LOG)
         compress_archive()
-        notify_failure(creds, exception, traceback.format_exc()) if exception else notify_success(creds)
+        if exception:
+            notify_failure(creds, exception, traceback.format_exc())
+        else:
+            notify_success(creds)
         execute("sudo poweroff")
     if exception:
         sys.exit(1)
--- nightly/start_verifier.py
+++ nightly/start_verifier.py
@@ -11,20 +11,25 @@ META_URL = "http://169.254.169.254/latest/meta-data/iam/"\
 VERIFIER_INSTANCE = "bounce-verifier"
 REGION = 'us-west-1'
 
+
 class Creds(object):
     def __init__(self, key, secret, token):
         self.key = key
         self.secret = secret
         self.token = token
 
+
 def get_aws_creds():
     props = json.load(urllib.urlopen(META_URL))
-    return Creds(props['AccessKeyId'], props['SecretAccessKey'], props['Token'])
+    return Creds(props['AccessKeyId'], props['SecretAccessKey'],
+                 props['Token'])
+
 
 def ensure_instance_state(name, creds, stop=False):
     conn = boto.ec2.connect_to_region(REGION, aws_access_key_id=creds.key,
-            aws_secret_access_key=creds.secret, security_token=creds.token)
-    instances = conn.get_only_instances(filters={"tag:Name" : name})
+                                      aws_secret_access_key=creds.secret,
+                                      security_token=creds.token)
+    instances = conn.get_only_instances(filters={"tag:Name": name})
     if not instances:
         raise RuntimeError("Instance not found!")
     instance = instances[0]
@@ -33,12 +38,14 @@ def ensure_instance_state(name, creds, stop=False):
     else:
         conn.start_instances([instance.id])
 
+
 def parse_args():
     parser = argparse.ArgumentParser(description='Manage the bounce verifier')
     parser.add_argument('--stop', dest='stop', action='store_true',
-        help='stop the instance')
+                        help='stop the instance')
     args = parser.parse_args()
     return args
+
 
 def main():
     args = parse_args()
