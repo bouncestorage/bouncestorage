ce917c60ce3e8133b0c05f7fccca5af05f0155aa
Ka-Hing Cheung <khc@bouncestorage.com>
2015-08-13 18:14:28 -0700

    setup docker dev to allow jmx and visualvm by default

--- bounce/docker_dev_entrypoint.sh
+++ bounce/docker_dev_entrypoint.sh
@@ -16,8 +16,15 @@ mkdir -p /data/jar
 pushd /data/jar
 jar xf /data/bounce.jar
 popd
-java -cp /data/assets:/data/classes:/data/jar com.bouncestorage.bounce.Main \
+java -Dcom.sun.management.jmxremote.authenticate=false \
+    -Dcom.sun.management.jmxremote.ssl=false \
+    -Dcom.sun.management.jmxremote.port=1101 \
+    -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 \
+    -cp /data/assets:/data/classes:/data/jar com.bouncestorage.bounce.Main \
     --properties /data/bounce.properties >& /var/log/bounce.log &
+
+jstatd -J-Djava.security.policy=jstatd.all.policy -p 1099 &
+
 
 child=$!
 wait "$child"
